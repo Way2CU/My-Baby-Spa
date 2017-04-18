db7932c5381ec5c9b76923b92358b5dc23dd03a6
iliyaM <iliya@way2cu.com>
2017-04-18 15:58:13 +0300

    Added ignore array for scroll function.

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -46,6 +46,33 @@ Site.is_mobile = function() {
 };
 
 /**
+ * Handle clicking on "start now" buttons.
+ * @param object event
+ */
+Site.handle_dialog_form = function(event) {
+	event.preventDefault();
+	Site.dialog_form.open();
+}
+
+/**
+ * Handle clicking on submit button in contact form dialog.
+ * @param object event
+ */
+Site.handle_submit_click = function(event) {
+	event.preventDefault();
+	Caracal.ContactForm.list[0]._form.trigger('submit');
+}
+
+/**
+ * Handle loading language constants from the server.
+ * @param object data
+ */
+Site.handle_language_load = function(data) {
+	Site.submit_button.innerText = data['send'];
+	Site.dialog_form.set_title(data['fill_out']);
+}
+
+/**
  * Function called when document and images have been completely loaded.
  */
 Site.on_load = function() {
@@ -54,15 +81,40 @@ Site.on_load = function() {
 
 	Site.header = document.querySelector('header');
 
-	window.addEventListener('scroll', function(){
-		if (window.scrollY === 0) {
-			Site.header.classList.remove('active');
-			console.log(window.scrollY);
-		} else {
-			Site.header.classList.add('active');
-		}
-	});
+	var ignore_paths = ['/gallery', '/contact'];
+
+	if (ignore_paths.indexOf(window.location.pathname) > -1) {
+		Site.header.classList.add('active');
+
+	} else {
+		// add class on scroll to header
+		window.addEventListener('scroll', function(){
+			if (window.scrollY === 0)
+				Site.header.classList.remove('active'); else
+				Site.header.classList.add('active');
+		});
+	}
+
+	//Dialog form
+	Site.dialog_form = new Caracal.Dialog();
+	Site.dialog_form.set_content_from_dom('div#floating_form');
+
+	Site.submit_button = document.createElement('a');
+	Site.submit_button.addEventListener('click', Site.handle_submit_click);
+	Site.dialog_form.add_control(Site.submit_button);
+
+	// load language constants from the server
+	language_handler.getTextArrayAsync(null, ['fill_out', 'send'], Site.handle_language_load);
+
+	//Dialog contact form button
+	Site.dialog_contact = document.querySelectorAll('a.contact_form');
+
+	// Conect event listners to buttons in nodelist
+	for(var i=0; i < Site.dialog_contact.length; i++) {
+		Site.dialog_contact[i].addEventListener('click', Site.handle_dialog_form);
+	}
 };
+
 
 
 // connect document `load` event with handler function
